b77fac5d1d4d84df88ecc2c6d7f7dc0eb8224002
Vlad <gapochka2000@mailru>
2021-02-24 20:56:21 +0300

    Лаба 3 задание 5

--- 3Лаба.pl
+++ 3Лаба.pl
@@ -8,3 +8,7 @@ fact(N,X):-N1 is N-1,fact(N1,X1),X is X1*N.
 fact(N,X):-factorial(N,X,1).
 factorial(1,X,X):-!.
 factorial(N,X,X1):-N1 is N-1,X2 is X1*N,factorial(N1,X,X2).
+fib(1,1):-!.
+fib(2,1):-!.
+fib(N,X):-N1 is N-1,N2 is N-2,fib(N1,X1),fib(N2,X2),X is X1+X2 .
+
