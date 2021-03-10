cf8d86b3527c7b92bfa94078990de85f4d198e3e
Vlad <gapochka2000@mailru>
2021-03-10 12:08:18 +0300

    2 Лаба 1,2 задание

---
+++ kek.pl
@@ -0,0 +1,247 @@
+heroes(albus_Dumbledore,1).
+heroes(bellatrix_Lestrange,1).
+heroes(harry_Potter,1).
+heroes(hermione_Granger,1).
+heroes(james_Potter,1).
+heroes(ginny_Weasley,1).
+heroes(draco_Malfoy,1).
+heroes(lily_Potter,1).
+heroes(lord_Voldemort,1).
+heroes(lucius_Malfoy,1).
+heroes(narcissa_Malfoy,1).
+heroes(remus_John_Lupin,1).
+heroes(ron_Weasley,1).
+heroes(severus_Snape,1).
+heroes(sirius_Black,1).
+heroes(fred_Weasley,0).
+heroes(george_Weasley,0).
+heroes(percy_Ignatius_Weasley,0).
+heroes(neville_Longbottom,0).
+heroes(vincent_Crabbe,0).
+heroes(gregory_Goyle,0).
+heroes(pansy_Parkinson,0).
+heroes(marcus_Flint,0).
+heroes(luna_Lovegood,0).
+heroes(cho_Chang,0).
+heroes(terry_Boot,0).
+heroes(michael_Corner,0).
+heroes(marietta_Edgecombe,0).
+heroes(cedric_Diggory,0).
+heroes(justin_Finch_letchley,0).
+heroes(ernest_Macmillan,0).
+heroes(zacharias_Smith,0).
+heroes(argus_Filch,0).
+
+faculty(albus_Dumbledore,1).
+faculty(bellatrix_Lestrange,2).
+faculty(harry_Potter,1).
+faculty(hermione_Granger,1).
+faculty(james_Potter,1).
+faculty(ginny_Weasley,1).
+faculty(draco_Malfoy,2).
+faculty(lily_Potter,1).
+faculty(lord_Voldemort,2).
+faculty(lucius_Malfoy,2).
+faculty(narcissa_Malfoy,2).
+faculty(remus_John_Lupin,1).
+faculty(ron_Weasley,1).
+faculty(severus_Snape,1).
+faculty(sirius_Black,1).
+faculty(fred_Weasley,1).
+faculty(george_Weasley,1).
+faculty(percy_Ignatius_Weasley,1).
+faculty(neville_Longbottom,1).
+faculty(vincent_Crabbe,2).
+faculty(gregory_Goyle,2).
+faculty(pansy_Parkinson,2).
+faculty(marcus_Flint,2).
+faculty(luna_Lovegood,3).
+faculty(cho_Chang,3).
+faculty(terry_Boot,3).
+faculty(michael_Corner,3).
+faculty(marietta_Edgecombe,3).
+faculty(cedric_Diggory,4).
+faculty(justin_Finch_letchley,4).
+faculty(ernest_Macmillan,4).
+faculty(zacharias_Smith,4).
+faculty(argus_Filch,0).
+
+purity_of_blood(albus_Dumbledore,2).
+purity_of_blood(bellatrix_Lestrange,1).
+purity_of_blood(harry_Potter,0).
+purity_of_blood(hermione_Granger,2).
+purity_of_blood(james_Potter,0).
+purity_of_blood(ginny_Weasley,0).
+purity_of_blood(draco_Malfoy,1).
+purity_of_blood(lily_Potter,0).
+purity_of_blood(lord_Voldemort,1).
+purity_of_blood(lucius_Malfoy,1).
+purity_of_blood(narcissa_Malfoy,1).
+purity_of_blood(remus_John_Lupin,2).
+purity_of_blood(ron_Weasley,0).
+purity_of_blood(severus_Snape,1).
+purity_of_blood(sirius_Black,2).
+purity_of_blood(fred_Weasley,0).
+purity_of_blood(george_Weasley,0).
+purity_of_blood(percy_Ignatius_Weasley,0).
+purity_of_blood(neville_Longbottom,0).
+purity_of_blood(vincent_Crabbe,2).
+purity_of_blood(gregory_Goyle,1).
+purity_of_blood(pansy_Parkinson,2).
+purity_of_blood(marcus_Flint,1).
+purity_of_blood(luna_Lovegood,0).
+purity_of_blood(cho_Chang,0).
+purity_of_blood(terry_Boot,0).
+purity_of_blood(michael_Corner,0).
+purity_of_blood(marietta_Edgecombe,0).
+purity_of_blood(cedric_Diggory,0).
+purity_of_blood(justin_Finch_letchley,2).
+purity_of_blood(ernest_Macmillan,1).
+purity_of_blood(zacharias_Smith,0).
+purity_of_blood(argus_Filch,0).
+
+patronus(albus_Dumbledore,1).
+patronus(bellatrix_Lestrange,1).
+patronus(harry_Potter,1).
+patronus(hermione_Granger,0).
+patronus(james_Potter,1).
+patronus(ginny_Weasley,0).
+patronus(draco_Malfoy,1).
+patronus(lily_Potter,1).
+patronus(lord_Voldemort,1).
+patronus(lucius_Malfoy,0).
+patronus(narcissa_Malfoy,1).
+patronus(remus_John_Lupin,1).
+patronus(ron_Weasley,1).
+patronus(severus_Snape,1).
+patronus(sirius_Black,1).
+patronus(fred_Weasley,0).
+patronus(george_Weasley,1).
+patronus(percy_Ignatius_Weasley,0).
+patronus(neville_Longbottom,0).
+patronus(vincent_Crabbe,1).
+patronus(gregory_Goyle,0).
+patronus(pansy_Parkinson,0).
+patronus(marcus_Flint,1).
+patronus(luna_Lovegood,0).
+patronus(cho_Chang,0).
+patronus(terry_Boot,1).
+patronus(michael_Corner,1).
+patronus(marietta_Edgecombe,0).
+patronus(cedric_Diggory,0).
+patronus(justin_Finch_letchley,1).
+patronus(ernest_Macmillan,0).
+patronus(zacharias_Smith,1).
+patronus(argus_Filch,0).
+
+gender_of_the_hero(albus_Dumbledore,1).
+gender_of_the_hero(bellatrix_Lestrange,0).
+gender_of_the_hero(harry_Potter,1).
+gender_of_the_hero(hermione_Granger,0).
+gender_of_the_hero(james_Potter,1).
+gender_of_the_hero(ginny_Weasley,0).
+gender_of_the_hero(draco_Malfoy,1).
+gender_of_the_hero(lily_Potter,0).
+gender_of_the_hero(lord_Voldemort,1).
+gender_of_the_hero(lucius_Malfoy,0).
+gender_of_the_hero(narcissa_Malfoy,0).
+gender_of_the_hero(remus_John_Lupin,1).
+gender_of_the_hero(ron_Weasley,1).
+gender_of_the_hero(severus_Snape,1).
+gender_of_the_hero(sirius_Black,1).
+gender_of_the_hero(fred_Weasley,1).
+gender_of_the_hero(george_Weasley,1).
+gender_of_the_hero(percy_Ignatius_Weasley,1).
+gender_of_the_hero(neville_Longbottom,1).
+gender_of_the_hero(vincent_Crabbe,1).
+gender_of_the_hero(gregory_Goyle,1).
+gender_of_the_hero(pansy_Parkinson,1).
+gender_of_the_hero(marcus_Flint,1).
+gender_of_the_hero(luna_Lovegood,0).
+gender_of_the_hero(cho_Chang,1).
+gender_of_the_hero(terry_Boot,1).
+gender_of_the_hero(michael_Corner,1).
+gender_of_the_hero(marietta_Edgecombe,0).
+gender_of_the_hero(cedric_Diggory,1).
+gender_of_the_hero(justin_Finch_letchley,1).
+gender_of_the_hero(ernest_Macmillan,1).
+gender_of_the_hero(zacharias_Smith,0).
+gender_of_the_hero(argus_Filch,1).
+
+age(albus_Dumbledore,6).
+age(bellatrix_Lestrange,5).
+age(harry_Potter,2).
+age(hermione_Granger,2).
+age(james_Potter,4).
+age(ginny_Weasley,2).
+age(draco_Malfoy,2).
+age(lily_Potter,4).
+age(lord_Voldemort,6).
+age(lucius_Malfoy,3).
+age(narcissa_Malfoy,3).
+age(remus_John_Lupin,4).
+age(ron_Weasley,0).
+age(severus_Snape,5).
+age(sirius_Black,5).
+age(fred_Weasley,3).
+age(george_Weasley,0).
+age(percy_Ignatius_Weasley,6).
+age(neville_Longbottom,2).
+age(vincent_Crabbe,3).
+age(gregory_Goyle,3).
+age(pansy_Parkinson,4).
+age(marcus_Flint,3).
+age(luna_Lovegood,0).
+age(cho_Chang,3).
+age(terry_Boot,2).
+age(michael_Corner,3).
+age(marietta_Edgecombe,2).
+age(cedric_Diggory,4).
+age(justin_Finch_letchley,5).
+age(ernest_Macmillan,6).
+age(zacharias_Smith,4).
+age(argus_Filch,3).
+
+question1(X1):-	write("Is the character the main character ?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X1).
+
+question2(X2):-	write("what is the hero's department?"),nl,
+                                write("4.Hufflepuff"),nl,
+                                write("3.Ravenclaw"),nl,
+                                write("2.Slytherin"),nl,
+				write("1.Gryffindor"),nl,
+				write("0. NO"),nl,
+				read(X2).
+
+question3(X3):-	write("what is the purity of the hero's blood?"),nl,
+				write("2. half-breed"),nl,
+				write("1. muggle-born"),nl,
+				write("0. pureblood"),nl,
+				read(X3).
+
+question4(X4):-	write(" Has the patronus been studied ?"),nl,
+				write("1.Yes"),nl,
+				write("0.No"),nl,
+				read(X4).
+
+question5(X5):-	write("what gender is the hero ?"),nl,
+				write("1. man"),nl,
+				write("0. woman"),nl,
+				read(X5).
+
+question6(X6):-	write("the age of the hero ?"),nl,
+                                write("6. 71-1000"),nl,
+                                write("5. 41-70"),nl,
+                                write("4. 31-40"),nl,
+                                write("3. 21-30"),nl,
+				write("1. 11-20"),nl,
+				write("0. 0-10"),nl,
+				read(X6).
+
+pr:-	question1(X1),question2(X2),question3(X3),question4(X4),
+		question5(X5),question6(X6),
+                heroes(X,X1),faculty(X,X2),purity_of_blood(X,X3),patronus(X,X4),
+		gender_of_the_hero(X,X5),age(X,X6),
+                write(X).
