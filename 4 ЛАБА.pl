9e5da387997b51adfdcd3c986d9f19b9d186ba1e
Vlad <gapochka2000@mailru>
2021-03-23 18:46:40 +0300

    Лаба 4 задание 2

--- 4 ЛАБА.pl
+++ 4 ЛАБА.pl
@@ -4,3 +4,9 @@ writeList([H|T]):-write(H),writeList(T).
 %Чтение списка
 readList(0,[]):-!.
 readList(N,[H|T]):-read(H),N1 is N-1,readList(N1,T).
+
+%Сумма элементов списка
+sum_list_down(List,Sum):-sum_list_down(List,0,Sum).
+sum_list_down([],Sum,Sum):-!.
+sum_list_down([H|T],S,Sum):-S1 is H + S,sum_list_down(T,S1,Sum).
+sum_list:-write("Введите количество элементов"),nl,read(N),write("Введите элементы списка:"),nl,readList(N,List),sum_list_down(List,Sum),write("Сумма = "),write(Sum).
