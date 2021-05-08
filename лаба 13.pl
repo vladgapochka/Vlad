1b69acc3b81fc0fe20c6a50ec7ffbe67ed0fe7e0
Vlad <gapochka2000@mail.ru>
2021-05-09 00:46:02 +0300

    Лаба 13 задание 3

--- лаба 13.pl
+++ лаба 13.pl
@@ -1,3 +1,5 @@
-fun main(args:Array<String>){
-    println("Hello world")
+fun main(args: Array<String>)
+{
+    val name = if (args.isNotEmpty()) args[0] else "Пользователи"
+    println("Привет ,$name")
 }
